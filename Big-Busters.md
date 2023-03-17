b37214e9eb273f0e9dbbc8fc9e993cc851a1843f
atulsingh369 <businessatul7684@gmail.com>
2023-03-17 19:44:47 +0530

    updated

---
+++ Big-Busters.md
@@ -0,0 +1,9 @@
+# Big Busters
+
+## Atul Kumar Singh (TX231361)
+
+## Atul Vaishya (TX231359)
+
+## Design and devlope an E-Learning plattform to provide easy and affordable education to the children of Rural Communities.
+
+[Repo Link](https://github.com/atulsingh369/Bug-Busters)
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("This is home page.");
+});
+
+app.post("/", (req, res) => {
+	res.send("This is home page with post request.");
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on PORT: ${PORT}`);
+});
