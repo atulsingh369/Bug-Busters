03fac3e5dbb9f177859279fea226872e60027865
atulsingh369 <businessatul7684@gmail.com>
2023-03-18 17:13:35 +0530

    updated navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import Header from './layout/Navbar'
 import Home from './pages/Home.js'
 import Footer from './layout/Footer'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 	return (
 		<>
-			<Router>
-				<Header />
-				<Routes>
-					<Route exact path="/" element={<Home />} />
-					{/* <Route exact path="/dashboard" element={<Dashboard />} />
+
+			<Header />
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				{/* <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/my-profile" element={<Profile />} /> */}
-				</Routes>
-				<Footer />
-			</Router>
+			</Routes>
+			<Footer />
+
 		</>
 	)
 }
